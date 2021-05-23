0fb80d270d53a190cedcc65f06dcbfe2557942a0
kottragu <alexklim0120@gmail.com>
2021-05-23 14:17:07 +0300

    replaced deprecated method

--- src/main/kotlin/com/plugin/kottragu/Notify.kt
+++ src/main/kotlin/com/plugin/kottragu/Notify.kt
@@ -1,7 +1,6 @@
 package com.plugin.kottragu
 
-import com.intellij.notification.NotificationDisplayType
-import com.intellij.notification.NotificationGroup
+import com.intellij.notification.NotificationGroupManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.StartupActivity
 import com.plugin.kottragu.service.TimerService
@@ -9,8 +8,7 @@ import com.plugin.kottragu.service.TimerService
 
 class Notify : StartupActivity {
     companion object {
-        private val STICKY_GROUP = NotificationGroup("demo.notifications.balloon",
-            NotificationDisplayType.STICKY_BALLOON, true)
+        private val STICKY_GROUP = NotificationGroupManager.getInstance().getNotificationGroup("notification")
     }
 
     override fun runActivity(project: Project) {
